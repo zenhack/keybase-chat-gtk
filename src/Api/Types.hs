9152a232005de18b19618ef7da337dfdb8044f6a
Ian Denhardt <ian@zenhack.net>
2019-07-14 20:06:36 -0400

    Work towards testing.
    ...and some misc styling and other stuff.
    
    Test doesn't build; it can't seem to find the FromJSON instance for
    ListResult, which is currently baffling to me.

--- src/Api/Types.hs
+++ src/Api/Types.hs
@@ -1,34 +1,51 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
 {-# LANGUAGE TemplateHaskell            #-}
 -- | Types that appear in the keybase chat API.
 module Api.Types
-    ( Conversation
+    ( ListResult(..)
+    , Conversation(..)
+    , Time(..)
+    , ConversationId(..)
+    , Channel(..)
+    , MembersType(..)
+    , TopicType(..)
+    , MsgId(..)
+    , Msg(..)
+    , Uid(..)
+    , DeviceId(..)
+    , Sender(..)
     ) where
 
 import Zhp
 
-import qualified Data.Aeson    as A
-import qualified Data.Aeson.TH as A
-import           Data.Text     (Text)
+import qualified Data.Aeson      as A
+import qualified Data.Aeson.TH   as A
+import           Data.List       (intercalate)
+import           Data.List.Extra (wordsBy)
+import           Data.Text       (Text)
+import           Prelude         (tail)
 
 newtype Time
     = Time Int64
-    deriving(A.ToJSON, A.FromJSON)
+    deriving(A.ToJSON, A.FromJSON, Show, Read, Eq, Ord, Bounded)
 
 data ListResult = ListResult
     { listresCoversations :: [Conversation]
     , listresOffline      :: Bool
     }
+    deriving(Show, Read, Eq)
 
 newtype ConversationId
     = ConversationId Text
-    deriving(A.ToJSON, A.FromJSON)
+    deriving(A.ToJSON, A.FromJSON, Show, Read, Eq, Ord)
 
 data Conversation = Conversation
     { convId      :: ConversationId
     , convChannel :: Channel
     , convUnread  :: Bool
     }
+    deriving(Show, Read, Eq)
 
 data Channel = Channel
     { chanName        :: Text
@@ -37,21 +54,22 @@ data Channel = Channel
     , chanTopicType   :: TopicType
     , chanTopicName   :: Maybe Text
     }
+    deriving(Show, Read, Eq)
 
 -- TODO: I(zenhack) assume there is a more specific set of values that MembersType
 -- and TopicType can take on; figure out what these are  and make them simple sum
 -- types instead of wrappers around Text.
 newtype MembersType
     = MembersType Text
-    deriving(A.ToJSON, A.FromJSON)
+    deriving(A.ToJSON, A.FromJSON, Show, Read, Eq, Ord)
 
 newtype TopicType
     = TopicType Text
-    deriving(A.ToJSON, A.FromJSON)
+    deriving(A.ToJSON, A.FromJSON, Show, Read, Eq, Ord)
 
 newtype MsgId
     = MsgId Int64
-    deriving(A.ToJSON, A.FromJSON)
+    deriving(A.ToJSON, A.FromJSON, Show, Read, Eq, Ord, Bounded)
 
 data Msg = Msg
     { msgId             :: MsgId
@@ -60,14 +78,15 @@ data Msg = Msg
     , msgSender         :: Sender
     , msgSentAt         :: Time
     }
+    deriving(Show, Read, Eq)
 
 newtype Uid
     = Uid Text
-    deriving(A.ToJSON, A.FromJSON)
+    deriving(A.ToJSON, A.FromJSON, Show, Read, Eq, Ord)
 
 newtype DeviceId
     = DeviceId Text
-    deriving(A.ToJSON, A.FromJSON)
+    deriving(A.ToJSON, A.FromJSON, Show, Read, Eq, Ord)
 
 data Sender = Sender
     { senderUid        :: Uid
@@ -75,19 +94,31 @@ data Sender = Sender
     , senderDeviceId   :: DeviceId
     , senderDeviceName :: Text
     }
+    deriving(Show, Read, Eq)
 
 -- Derive JSON type classes:
-do
-    let mangleFieldLabel = dropPrefix >>> toSnakeCase
-        dropPrefix = dropWhile isLower
-        toSnakeCase = id -- TODO
-        cfg = A.defaultOptions
-            { A.fieldLabelModifier = mangleFieldLabel
-            , A.omitNothingFields = True
-            }
+do  let drv =
+            A.deriveJSON A.defaultOptions
+                { A.fieldLabelModifier = mangleFieldLabel
+                , A.omitNothingFields = True
+                }
+
+        mangleFieldLabel =
+            toSnakeCase >>> dropPrefix
+
+        toSnakeCase "" = ""
+        toSnakeCase (c:cs)
+            | isUpper c = '_' : toLower c : toSnakeCase cs
+            | otherwise = c : toSnakeCase cs
+
+        dropPrefix =
+            wordsBy (== '_')
+            >>> tail
+            >>> intercalate "_"
+
     -- keep these sorted:
-    A.deriveJSON cfg 'Channel
-    A.deriveJSON cfg 'Conversation
-    A.deriveJSON cfg 'ListResult
-    A.deriveJSON cfg 'Msg
-    A.deriveJSON cfg 'Sender
+    drv 'Channel
+    drv 'Conversation
+    drv 'ListResult
+    drv 'Msg
+    drv 'Sender
--- tests/Main.hs
+++ tests/Main.hs
@@ -2,5 +2,12 @@ module Main (main) where
 
 import Zhp
 
+import Api.Types
+
+import qualified Data.Aeson           as A
+import qualified Data.ByteString.Lazy as LBS
+
 main :: IO ()
-main = pure ()
+main = do
+    resultBytes <- LBS.readFile "testdata/list-result.json"
+    print (A.decode resultBytes :: Maybe ListResult)

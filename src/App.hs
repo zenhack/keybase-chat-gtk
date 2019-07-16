72944807624c1888ecf6ff70e5f232b1be82d59f
Ian Denhardt <ian@zenhack.net>
2019-07-15 23:06:38 -0400

    Gtk Hello, World!

--- src/App.hs
+++ src/App.hs
@@ -1,6 +1,33 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedLists  #-}
 module App (main) where
 
 import Zhp
 
+import GI.Gtk                        hiding ((:=), main, on)
+import GI.Gtk.Declarative
+import GI.Gtk.Declarative.App.Simple
+
+data Model = Model
+
+data Msg
+    = Quit
+
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = void $ run App
+    { view = view'
+    , update = update'
+    , inputs = []
+    , initialState = Model
+    }
+
+update' :: Model -> Msg -> Transition Model Msg
+update' Model Quit = Exit
+
+view' :: Model -> AppView Window Msg
+view' Model =
+    bin Window
+        [ #title := "Hello, Keybase!"
+        , on #destroy Quit
+        ]
+        $ widget Label [#label := "TODO: write the app"]

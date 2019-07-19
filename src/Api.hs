8e7cf8284597bed65b784ea10aea094cbaeca014
Ian Denhardt <ian@zenhack.net>
2019-07-18 23:19:49 -0400

    First pass at making api calls, proper test.

--- src/Api.hs
+++ src/Api.hs
@@ -3,6 +3,8 @@
 -- For this to work, the 'keybase' command line tool must be in $PATH;
 -- this operates by launching `keybase chat api` and talking to it via its
 -- stdio.
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Api
     ( -- * Connecting to the API.
       Conn
@@ -11,15 +13,32 @@ module Api
 
 import Zhp
 
+import Control.Concurrent.MVar (MVar, newMVar, withMVar)
 import Control.Monad.IO.Unlift (MonadUnliftIO)
+import GHC.Generics            (Generic)
 import System.Process.Typed
-    (Process, createPipe, proc, setStdin, setStdout, withProcessWait)
+    ( Process
+    , createPipe
+    , getStdin
+    , getStdout
+    , proc
+    , setStdin
+    , setStdout
+    , withProcessWait
+    )
 
-import qualified Data.Aeson as A
+import qualified Api.Types as AT
+
+import           Data.Aeson           ((.=))
+import qualified Data.Aeson           as A
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Text            as T
+import qualified Data.Text.Encoding   as TE
 
 -- A Connection to the API
-newtype Conn = Conn
+data Conn = Conn
     { connApiProc :: Process Handle Handle ()
+    , connMutex   :: MVar ()
     }
 
 -- | @'withConn' f@ connects to the API, runs the action passing it the connection,
@@ -30,6 +49,40 @@ withConn f =
             & setStdin createPipe
             & setStdout createPipe
     in
-    withProcessWait cfg $ \proc -> f (Conn proc)
+    withProcessWait cfg $ \proc -> do
+        mu <- liftIO $ newMVar ()
+        f Conn
+            { connApiProc = proc
+            , connMutex = mu
+            }
 
-data MethodCall = MethodCall
+call :: (MonadIO m, A.ToJSON req, A.FromJSON resp) => Conn -> req -> m resp
+call Conn{ connApiProc = proc, connMutex = mu } req =
+    liftIO $ withMVar mu $ \_ -> do
+        LBS.hPut (getStdin proc) (A.encode req <> "\n")
+        hFlush (getStdin proc)
+        -- XXX: this is pretty gross; ideally we'd just read in the line
+        -- and hand it straight to decode, but I can't find a function that
+        -- reads a line and returns it as a ByteString.
+        line <- hGetLine (getStdout proc)
+        case A.decodeStrict $ TE.encodeUtf8 (T.pack line) of
+            Just x  -> pure x
+            Nothing -> error "TODO: throw a proper exception"
+
+data MethodCall param = MethodCall
+    { method :: T.Text
+    , param  :: param
+    }
+    deriving(Generic)
+
+instance A.FromJSON a => A.FromJSON (MethodCall a)
+instance A.ToJSON a => A.ToJSON (MethodCall a)
+
+newtype MethodReturn ret = MethodReturn
+    { result :: ret
+    }
+    deriving(A.FromJSON, A.ToJSON)
+
+list :: MonadIO m => Conn -> m AT.ListResult
+list conn =
+    result <$> call conn (A.object ["method" .= A.String "list"])
--- tests/Main.hs
+++ tests/Main.hs
@@ -2,12 +2,45 @@ module Main (main) where
 
 import Zhp
 
+import Test.Hspec
+
 import Api.Types
 
 import qualified Data.Aeson           as A
 import qualified Data.ByteString.Lazy as LBS
 
+
+result = ListResult
+    { listresConversations =
+        [ Conversation
+            { convId = ConversationId "abcdef01235390abe0320abc02ff2a0b0acef0a32376816ab3298500ca4329af"
+            , convChannel = Channel
+                { chanName = "crypto_nerds"
+                , chanPublic = False
+                , chanMembersType = MembersType "team"
+                , chanTopicType = TopicType "chat"
+                , chanTopicName = Just "general"
+                }
+            , convUnread = False
+            }
+        , Conversation
+            { convId = ConversationId "3782956167bacde1349fa23c389132e89a3289f93106809cef92039508b3289b"
+            , convChannel = Channel
+                { chanName = "alice,bob"
+                , chanPublic = False
+                , chanMembersType = MembersType "impteamnative"
+                , chanTopicType = TopicType "chat"
+                , chanTopicName = Nothing
+                }
+            , convUnread = False
+            }
+        ]
+    , listresOffline = False
+    }
+
 main :: IO ()
-main = do
-    resultBytes <- LBS.readFile "testdata/list-result.json"
-    print (A.decode resultBytes :: Maybe ListResult)
+main = hspec $ do
+    describe "decoding resutlts" $ do
+        it "Should decode the test data correctly" $ do
+            resultBytes <- LBS.readFile "testdata/list-result.json"
+            A.decode resultBytes `shouldBe` Just result
